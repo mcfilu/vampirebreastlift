be91f47c39d9220c96baaf8b0f1d7bc4449206ba
mcfilu <filip.1olszewski@gmail.com>
2023-06-22 00:58:39 +0100

    adding the remaining new files

---
+++ src/components/TrustBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const TrustBox = () => {
+  
+  return (
+<div class="trustpilot-widget" data-locale="en-US" data-template-id="53aa8912dec7e10d38f59f36" data-businessunit-id="53da56ff0000640005792dae" data-style-height="140px" data-style-width="100%" data-theme="light" data-stars="4,5">
+  <a href="https://www.trustpilot.com/review/bluevine.com" target="_blank">Trustpilot</a>
+</div>
+  );
+};
+export default TrustBox;
