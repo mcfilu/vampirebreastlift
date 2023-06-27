be50357f92403426a8cacb1d607c792672f56e45
mcfilu <filip.1olszewski@gmail.com>
2023-06-27 23:45:21 +0100

    test

--- src/components/FooterBook.jsx
+++ src/components/FooterBook.jsx
@@ -7,7 +7,7 @@ function FooterBook() {
             <p className={`text-white text-[2.5vh]`}>We offer 0% finance on all our treatments, please call the clinic to find out more</p>
             <p href="tel:5005355343" className={`text-gold1 ml-[1vh] text-[2.5vh]`}>+44 (0)20 3006 8459</p>
         </div>
-        
+        {/* sjfksdkf */}
         <button className={`bg-gold1 text-[3vh] h-full px-[1vw]`}>Book Consultation</button>
     </div>
   )
